2833b1b959d303fb7e9726f11d0576d2de618ef8
ed85w <ed.walker@hotmail.co.uk>
2018-11-22 22:05:57 +0000

    after lesson 14

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
-new Vue ({
-  el: '#vue-app',
+var one = new Vue ({
+  el: '#vue-app-one',
   data: {
-    health: 100,
-    medMealth: false,
-    lowHealth: false,
-    ended: false
+    title: 'Vue App One'
   },
   methods: {
-    punch: function(){
-      this.health -= 10;
-      if(this.health<= 0){
-        this.ended = true;
-      }
-      if(this.health > 30 && this.health < 80){
-        this.medHealth = true;
-      }
-      if(this.health <= 30){
-        this.lowHealth = true;
-      }
-    },
-    restart: function(){
-      this.health = 100;
-      this.ended = false;
-      this.medHealth = false;
-      this.lowHealth = false;
-    }
   },
   computed: {
-
+    greet: function(){
+      return 'Hello from app one';
+    }
   }
 
 });
+
+var two = new Vue ({
+  el: '#vue-app-two',
+  data: {
+    title: 'Vue App Two'
+  },
+  methods: {
+    changeTitle: function(){
+      one.title = "Title changed!"
+    }
+  },
+  computed: {
+    greet: function(){
+      return 'Hello from app two';
+    }
+  }
+
+});
+
+// properties can also be accessed/changed from outside vue instance
+two.title = "Changed from outside";
