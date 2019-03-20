6358850caf84b77733342442fadc78830903e4b1
ed85w <ed.walker@hotmail.co.uk>
2019-03-20 21:01:59 +0000

    after lesson 29

--- vuejs-test/src/App.vue
+++ vuejs-test/src/App.vue
@@ -1,26 +1,20 @@
 <template>
   <div>
-    <!-- keep alive retains data when switching between components -->
-    <keep-alive>
-    <component v-bind:is="component"></component>
-    </keep-alive>
-    <button v-on:click="component = 'form-one'">Show form 1</button>
-    <button v-on:click="component = 'form-two'">Show form 2</button>
+    <add-blog></add-blog>
   </div>
 </template>
 
 <script>
-import formOne from './components/formOne.vue'
-import formTwo from './components/formTwo.vue'
+
+import addBlog from './components/addBlog.vue'
 
 export default {
   components: {
-    'form-one': formOne,
-    'form-two': formTwo
-  },
-  data(){
-    return {
-      component: 'form-two'
+    'add-blog': addBlog
+  },  
+  data() {
+    return{
+
     }
   },
   methods: {
@@ -29,6 +23,11 @@ export default {
 }
 </script>
 
-<style> 
+<style>
+
+body {
+  margin: 0;
+}
 
 </style>
+
