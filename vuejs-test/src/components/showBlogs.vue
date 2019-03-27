0a877696141079cd56f9258e5838160900760c92
ed85w <ed.walker@hotmail.co.uk>
2019-03-27 20:51:15 +0000

    after lesson 33

---
+++ vuejs-test/src/components/showBlogs.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="show-blogs">
+    <h1>All Blog Articles</h1>
+    <div v-for="blog in blogs" class="single-blog">
+      <h2>{{blog.title}}</h2>
+      <article>{{blog.body}}</article>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return{
+      // empty array for storing data from get
+      blogs: []
+    }
+  },
+  methods: {
+
+  },
+  created(){ //runs when page created 
+    this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data){
+      console.log(data);
+      //get first 10 instances of array (body property)
+      this.blogs = data.body.slice(0,10)
+    })
+  }
+}
+</script>
+
+<style>
+
+#show-blogs {
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+.single-blog {
+  padding: 20px;
+  margin: 20px 0;
+  box-sizing: border-box;
+  background: #eee;
+}
+
+
+</style>
+
