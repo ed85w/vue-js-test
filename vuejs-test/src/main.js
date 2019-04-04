bba52b79f80caa096c976ff6731ce00f66a27ea5
ed85w <ed.walker@hotmail.co.uk>
2019-04-04 21:36:38 +0100

    after lesson 39, setting up routing

--- vuejs-test/src/main.js
+++ vuejs-test/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+import Routes from './routes'
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: Routes
+});
 
 // CUSTOM DIRECTIVES
 
@@ -44,6 +51,7 @@ Vue.filter('snippet', function(value){
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router: router
 })
 
